e0b47aeb40b0b42c70004f4800c04f21ec2cbc3e
root <root@IVAN.MBP.COM>
2023-01-09 15:39:28 -0300

    WIP Unit test of services

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -8,6 +8,7 @@ use App\Messenger\Message\UserRegisteredMessage;
 use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
+use Doctrine\ORM\Exception\ORMException;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -31,7 +32,7 @@ class UserRegisterService
 
         try {
             $this->userRepository->save($user);
-        } catch (\Exception $exception) {
+        } catch (ORMException $e) {
             throw UserAlreadyExistException::fromEmail($email);
         }
 
