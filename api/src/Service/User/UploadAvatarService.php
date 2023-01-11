5cc09d81f04a03f6f854b22f07167db5eb3629b3
root <root@IVAN.MBP.COM>
2023-01-11 15:58:41 -0300

    Adding action and services logic (1)

--- api/src/Service/User/UploadAvatarService.php
+++ api/src/Service/User/UploadAvatarService.php
@@ -3,16 +3,27 @@
 namespace App\Service\User;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Service\File\FileService;
 use Symfony\Component\HttpFoundation\Request;
 
 class UploadAvatarService
 {
-    public function __construct()
+    private UserRepository $userRepository;
+    private FileService $fileService;
+    private string $mediaPath;
+
+    public function __construct(UserRepository $userRepository, FileService $fileService, string $mediaPath)
     {
+
+        $this->userRepository = $userRepository;
+        $this->fileService = $fileService;
+        $this->mediaPath = $mediaPath;
     }
 
     public function uploadAvatar(Request $request, User $user): User
     {
+        $file = $this->fileService->validateFile($request, FileService::AVATAR_INPUT_NAME);
 
     }
 }
